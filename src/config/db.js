574f8c1eab19c3b842dc9dfeb11b9aa336864269
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-17 17:05:31 +0530

    complete project

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,25 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const isLocalhost = process.env.DB_HOST === "localhost";
+const isLocalhost =
+  process.env.DB_HOST === "localhost" || process.env.DB_HOST === "127.0.0.1";
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   ssl: isLocalhost ? false : { rejectUnauthorized: false },
 });
 
 pool.on("connect", () => {
-  console.log("Connected to the PostgreSQL database");
+  console.log("Connected to Aiven PostgreSQL");
 });
 
 pool.on("error", (err) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
+  console.error("Unexpected PostgreSQL error", err);
+  process.exit(1);
 });
 
 module.exports = {
